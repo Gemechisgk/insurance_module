9c1b6172cf66d1ef4549281026cffacf1e090445
GemechisGk <gemechis.gk@gmail.com>
2025-04-27 16:52:49 +0300

    feat: Enhance ResConfigSettings with parameter management for insurance modules
    - Added set_values and get_values methods to manage configuration parameters for various insurance-related modules.
    - Enabled saving and retrieving settings for auto inspection reminders, safety dashboards, insurance claim automation, analytics, vehicle maintenance reminders, and safety training management.

--- models/res_config_settings.py
+++ models/res_config_settings.py
@@ -26,4 +26,25 @@ class ResConfigSettings(models.TransientModel):
     module_safety_training_management = fields.Boolean(
         string='Safety Training Management',
         help='Enable safety training management'
-    ) 
+    )
+
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('insurance_module.module_auto_inspection_reminder', self.module_auto_inspection_reminder)
+        self.env['ir.config_parameter'].sudo().set_param('insurance_module.module_safety_dashboard', self.module_safety_dashboard)
+        self.env['ir.config_parameter'].sudo().set_param('insurance_module.module_insurance_claim_automation', self.module_insurance_claim_automation)
+        self.env['ir.config_parameter'].sudo().set_param('insurance_module.module_insurance_analytics', self.module_insurance_analytics)
+        self.env['ir.config_parameter'].sudo().set_param('insurance_module.module_vehicle_maintenance_reminder', self.module_vehicle_maintenance_reminder)
+        self.env['ir.config_parameter'].sudo().set_param('insurance_module.module_safety_training_management', self.module_safety_training_management)
+
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            module_auto_inspection_reminder=self.env['ir.config_parameter'].sudo().get_param('insurance_module.module_auto_inspection_reminder', default=False),
+            module_safety_dashboard=self.env['ir.config_parameter'].sudo().get_param('insurance_module.module_safety_dashboard', default=False),
+            module_insurance_claim_automation=self.env['ir.config_parameter'].sudo().get_param('insurance_module.module_insurance_claim_automation', default=False),
+            module_insurance_analytics=self.env['ir.config_parameter'].sudo().get_param('insurance_module.module_insurance_analytics', default=False),
+            module_vehicle_maintenance_reminder=self.env['ir.config_parameter'].sudo().get_param('insurance_module.module_vehicle_maintenance_reminder', default=False),
+            module_safety_training_management=self.env['ir.config_parameter'].sudo().get_param('insurance_module.module_safety_training_management', default=False)
+        )
+        return res 
